e5760f788b6356874b5f3938381c15dfe86ec06f
JM <jm@JMdeMacBook-Air.local>
2018-12-24 17:15:11 +0800

    vue-steps
    增加示例图
    
    Update README.md
    
    step.gif

--- README.md
+++ README.md
@@ -14,21 +14,3 @@ npm run serve
 ```
 npm run build
 ```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/VueSteps.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="the-steps">
+    <div class="the-steps-item" v-for="(item, index) in items" :key="index">
+      <div :class="['the-steps-item-num', activeIndex >= index ? 'activeImage' : '']">
+        <span v-if="activeIndex < index">{{item.num}}</span>
+      </div>
+      <div :class="['the-steps-item-text', activeIndex >= index ? 'activeText' : '']">{{item.text}}</div>
+      <div :class="['the-steps-item-line', activeIndex >= index ? 'activeLine' : '']"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueSteps',
+  props: {
+    items: {
+      type: Array
+    },
+    activeIndex: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+.the-steps {
+  width: 100%;
+  display: flex;
+  &-item {
+    display: flex;
+    width: 100%;
+    height: 34px;
+    &:last-child {
+      width: 45%;
+      .the-steps-item-line {
+        display: none;
+      }
+    }
+    &-num {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      line-height: 30px;
+      color: #00000072;
+      text-align: center;
+    }
+    &-text {
+      color: #00000072;
+      font-size: 16px;
+      line-height: 30px;
+      margin-left: 10px;
+    }
+    &-line {
+      flex: 1;
+      height: 1px;
+      background: #ccc;
+      margin: 15px 15px 0;
+    }
+
+    .activeImage {
+      width: 30px;
+      height: 30px;
+      background: url(../assets/icon-success.png) no-repeat;
+      background-size: 100%;
+    }
+    .activeText {
+      color: #000000d8;
+      font-size: 16px;
+      font-weight: 700;
+    }
+    .activeLine {
+      color: #fff;
+      background: #1890ff;
+    }
+  }
+}
+</style>
